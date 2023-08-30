fbea6dc0944b2d51b36da34af8bd904b5a21e2f3
sire <robertcxlee@gmail.com>
2023-08-30 17:20:56 -0600

    added borders

--- components/dumpster/Folder.js
+++ components/dumpster/Folder.js
@@ -1,6 +1,6 @@
 export default function Folder(...props) {
 	return (
-		<button className="m-[0.5rem] min-h-[4.5rem] max-h-[4.5rem] min-w-[4.5rem] max-w-[4.5rem] p-[0.5rem] bg-dark-contrast rounded">
+		<button className="m-[0.5rem] min-h-[4.5rem] max-h-[4.5rem] min-w-[4.5rem] max-w-[4.5rem] p-[0.5rem] bg-dark-contrast box rounded">
 			<img src="/images/folder.png" />
 		</button>
 	);
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Dumpster from "./../components/dumpster/Dumpster";
 
 export default function Home() {
 	return (
-		<div className="px-[2rem] py-[1rem] bg-dark-pitch justify-center items-center flex flex-col">
+		<div className="px-[2rem] py-[1rem] bg-dark-pitch">
 			<div className="justify-center flex items-center mb-[0.5rem] overflow-x-hidden">
 				<img src="/images/icon.png" width="100" height="100" />
 				<h1 className="text-blue-base text-2xl font-extrabold mb-4">
