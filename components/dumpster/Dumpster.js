2ae8bc1bb30a807c661468c09c07954e14ec37e2
sire <robertcxlee@gmail.com>
2023-08-29 18:19:02 -0600

    added basic scroll

--- components/dumpster/Dumpster.js
+++ components/dumpster/Dumpster.js
@@ -1,3 +1,22 @@
 export default function Dumpster() {
-	return <div className="bg-dark-base rounded py-[8rem] px-[8rem]"></div>;
+	return (
+		<div className="bg-dark-base rounded py-[2rem] px-[8rem] overflow-scroll min-h-[24rem] max-h-[24rem]">
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+			<h1 className="text-lg text-blue-highlight">test</h1>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Dumpster from "./../components/dumpster/Dumpster";
 export default function Home() {
 	return (
 		<div className="px-[2rem] py-[1rem] bg-dark-pitch">
-			<div className="justify-center flex items-center mb-[0.5rem]">
+			<div className="justify-center flex items-center mb-[0.5rem] overflow-x-hidden">
 				<img src="/images/icon.png" width="100" height="100" />
 				<h1 className="text-blue-base text-2xl font-extrabold mb-4">
 					pageDump
