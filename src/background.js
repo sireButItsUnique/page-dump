f474e25725da038b593432f806c85fd74cf5fb62
sire <robertcxlee@gmail.com>
2023-10-18 21:48:26 -0600

    added command hotkey

--- src/background.js
+++ src/background.js
@@ -1,1 +1,14 @@
-console.log("foo");
+async function getCurrentTab() {
+	let queryOptions = { active: true, lastFocusedWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	return tab;
+}
+
+chrome.commands.onCommand.addListener(async (command) => {
+	if (command === "save") {
+		let tab = await getCurrentTab();
+		console.log("saved " + tab.url);
+	} else if (command === "random") {
+		console.log("random");
+	}
+});
