8e656ca2f00a82e2b22bffd5eaca8be5564529df
sire <robertcxlee@gmail.com>
2023-10-22 18:42:05 -0600

    refactored code to work with states

--- src/folder.js
+++ src/folder.js
@@ -1,60 +1,48 @@
-function addFolder() {
-	// create folder
-	let div = document.createElement("div");
-	div.className = "m-[0.5rem] flex flex-col justify-center";
+class Folder {
+	constructor(parentId) {
+		// create folder
+		let div = document.createElement("div");
+		div.className = "m-[0.5rem] flex flex-col justify-center";
 
-	let folder = document.createElement("button");
-	folder.className =
-		"min-h-[4.5rem] max-h-[4.5rem] min-w-[4.5rem] max-w-[4.5rem] p-[0.5rem] bg-dark-contrast box rounded grid relative";
+		let folder = document.createElement("button");
+		folder.className =
+			"min-h-[4.5rem] max-h-[4.5rem] min-w-[4.5rem] max-w-[4.5rem] p-[0.5rem] bg-dark-contrast box rounded grid relative";
 
-	let img = document.createElement("img");
-	img.src = "./../public/images/folder.png";
+		let img = document.createElement("img");
+		img.src = "./../public/images/folder.png";
 
-	let label = document.createElement("h1");
-	label.className = "mt-[0.25rem] text-blue-base text-center";
-	label.innerText = "Folder";
+		let label = document.createElement("h1");
+		label.className = "mt-[0.25rem] text-blue-base text-center";
+		label.innerText = "Folder";
 
-	// add functionality
-	folder.addEventListener("contextmenu", (e) => {
-		e.preventDefault();
-		let menu = document.createElement("div");
-		menu.className =
-			"bg-dark-contrast border-slate-400 border rounded left-[40%] top-[80%] px-[0.1rem] py-[0.25rem] absolute z-[10]";
+		// add functionality
+		folder.addEventListener("contextmenu", (e) => {
+			e.preventDefault();
+			let menu = document.createElement("div");
+			menu.className =
+				"bg-dark-contrast border-slate-400 border rounded left-[40%] top-[80%] px-[0.1rem] py-[0.25rem] absolute z-[10]";
 
-		let trash = document.createElement("button");
-		trash.innerText = "Delete";
-		trash.className =
-			"text-red-400 hover:bg-dark-pitch rounded-sm px-[0.4rem]";
+			let trash = document.createElement("button");
+			trash.innerText = "Delete";
+			trash.className =
+				"text-red-400 hover:bg-dark-pitch rounded-sm px-[0.4rem]";
 
-		let rename = document.createElement("button");
-		rename.innerText = "Rename";
-		rename.className =
-			"text-slate-500 hover:bg-dark-pitch rounded-sm px-[0.4rem]";
+			let rename = document.createElement("button");
+			rename.innerText = "Rename";
+			rename.className =
+				"text-slate-500 hover:bg-dark-pitch rounded-sm px-[0.4rem]";
 
-		menu.appendChild(rename);
-		menu.appendChild(trash);
-		folder.appendChild(menu);
-	});
+			menu.appendChild(rename);
+			menu.appendChild(trash);
+			folder.appendChild(menu);
+		});
 
-	// inject into dom
-	folder.appendChild(img);
-	div.appendChild(folder);
-	div.appendChild(label);
-	document.getElementById("dumpster").appendChild(div);
+		// inject into dom
+		folder.appendChild(img);
+		div.appendChild(folder);
+		div.appendChild(label);
+		document.getElementById(parentId).appendChild(div);
+	}
 }
 
-// handle events to create new folders
-addFolder();
-chrome.storage.local.get(["cnt"]).then((res) => {
-	while (res.cnt--) {
-		addFolder();
-	}
-});
-
-chrome.storage.onChanged.addListener((changes, namespace) => {
-	if (namespace == "local" && changes.cnt?.newValue) {
-		chrome.storage.local.get(["cnt"]).then((res) => {
-			addFolder();
-		});
-	}
-});
+export default Folder;
--- src/script.js
+++ src/script.js
@@ -1,34 +1,17 @@
-function add(data) {
-	let div = document.createElement("div");
-	let label = document.createElement("h1");
-	label.textContent = "bookmarking " + data;
-	div.style.zIndex = 999;
-	div.style.position = "fixed";
-	div.style.top = "50%";
-	div.style.left = "50%";
-	div.style.transform = "translate(-50%, -50%)";
-	div.style.backgroundColor = "#101519";
-	div.style.borderRadius = "2px";
-	div.style.paddingRight = "1rem";
-	div.style.paddingLeft = "1rem";
-	div.style.paddingTop = "0.5rem";
-	div.style.paddingBottom = "0.5rem";
-	label.style.color = "#2e7b94";
-	label.style.fontSize = "1rem";
-	label.style.fontFamily =
-		"Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji";
-	div.appendChild(label);
-	document.body.appendChild(div);
-}
+import Folder from "./folder.js";
 
-chrome.storage.local.get(["saved"]).then((res) => {
-	//add(res.saved);
+// handle events to create new folders
+let folder = new Folder("dumpster");
+chrome.storage.local.get(["cnt"]).then((res) => {
+	while (res.cnt--) {
+		let folder = new Folder("dumpster");
+	}
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-	if (namespace == "local" && changes.saved?.newValue) {
-		chrome.storage.local.get(["saved"]).then((res) => {
-			//add(res.saved);
+	if (namespace == "local" && changes.cnt?.newValue) {
+		chrome.storage.local.get(["cnt"]).then((res) => {
+			let folder = new Folder("dumpster");
 		});
 	}
 });
