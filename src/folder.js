2f3d73196b7e09c56e72d8d91e65a1c0e4f3c90d
sire <robertcxlee@gmail.com>
2023-10-22 18:10:36 -0600

    added right click menu

--- src/folder.js
+++ src/folder.js
@@ -1,22 +1,46 @@
 function addFolder() {
 	// create folder
+	let div = document.createElement("div");
+	div.className = "m-[0.5rem] flex flex-col justify-center";
+
 	let folder = document.createElement("button");
-	let img = document.createElement("img");
 	folder.className =
-		"m-[0.5rem] min-h-[4.5rem] max-h-[4.5rem] min-w-[4.5rem] max-w-[4.5rem] p-[0.5rem] bg-dark-contrast box rounded";
+		"min-h-[4.5rem] max-h-[4.5rem] min-w-[4.5rem] max-w-[4.5rem] p-[0.5rem] bg-dark-contrast box rounded grid relative";
+
+	let img = document.createElement("img");
 	img.src = "./../public/images/folder.png";
+
+	let label = document.createElement("h1");
+	label.className = "mt-[0.25rem] text-blue-base text-center";
+	label.innerText = "Folder";
 
 	// add functionality
 	folder.addEventListener("contextmenu", (e) => {
 		e.preventDefault();
-		let label = document.createElement("h1");
-		label.innerText = "wtfdahkj";
-		folder.appendChild(label);
+		let menu = document.createElement("div");
+		menu.className =
+			"bg-dark-contrast border-slate-400 border rounded left-[40%] top-[80%] px-[0.1rem] py-[0.25rem] absolute z-[10]";
+
+		let trash = document.createElement("button");
+		trash.innerText = "Delete";
+		trash.className =
+			"text-red-400 hover:bg-dark-pitch rounded-sm px-[0.4rem]";
+
+		let rename = document.createElement("button");
+		rename.innerText = "Rename";
+		rename.className =
+			"text-slate-500 hover:bg-dark-pitch rounded-sm px-[0.4rem]";
+
+		menu.appendChild(rename);
+		menu.appendChild(trash);
+		folder.appendChild(menu);
 	});
 
 	// inject into dom
 	folder.appendChild(img);
-	document.getElementById("dumpster").appendChild(folder);
+	div.appendChild(folder);
+	div.appendChild(label);
+	document.getElementById("dumpster").appendChild(div);
 }
 
 // handle events to create new folders
